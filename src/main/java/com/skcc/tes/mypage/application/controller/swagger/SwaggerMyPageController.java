262198f8666c6c5e31b6c32129a4e824e374a47b
yongjun <ruzun88@gmail.com>
2022-06-17 02:10:50 +0900

    ingress header

--- src/main/java/com/skcc/tes/mypage/application/controller/swagger/SwaggerMyPageController.java
+++ src/main/java/com/skcc/tes/mypage/application/controller/swagger/SwaggerMyPageController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/mypage")
+@RequestMapping("/my-page")
 public class SwaggerMyPageController extends MyPageController {
     public SwaggerMyPageController(MypageServicePort mypageService) {
         super(mypageService);
